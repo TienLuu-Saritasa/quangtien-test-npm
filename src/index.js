0f49d18a65e5535d58f9beab7a58c85497c41ca1
TienLuu-Saritasa <tien.luu@saritasa.com>
2021-08-19 12:40:38 +0700

    Init package

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const cors = require("cors");
+const env = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+env.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.start = (port) => {
+  app.listen(port, () => {
+    console.log("Server run on " + port)
+  });
+}
+
+module.exports = app;
