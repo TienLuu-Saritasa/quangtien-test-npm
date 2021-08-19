e6e4fe41729f2cd12e31913a47d0fbf68744f8cf
TienLuu-Saritasa <tien.luu@saritasa.com>
2021-08-19 13:27:48 +0700

    test-npm chalk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const env = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
+const chalk = require('chalk');
 
 const app = express();
 env.config();
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.start = (port) => {
   app.listen(port, () => {
-    console.log("Server run on " + port)
+    console.log(chalk.green.underline.bold("Server run on " + port));
   });
 }
 
